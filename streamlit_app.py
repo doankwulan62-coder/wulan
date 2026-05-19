709b5297bafde454d13a3a1b26529c4cbb588b8c
doankwulan62-coder <doankwulan62@gmail.com>
2026-05-19 11:27:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈 aplikasi ankim kalcer")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 import streamlit as st
-
+)
 number = st.number_input(
     "Insert a number", value=None, placeholder="Type a number..."
 )
