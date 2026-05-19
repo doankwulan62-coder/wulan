175a9a35d6ccc5d4fb7561d057952dfc379a7c4c
doankwulan62-coder <doankwulan62@gmail.com>
2026-05-19 11:27:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,9 @@ import streamlit as st
 st.title("🎈 aplikasi ankim kalcer")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 import streamlit as st
 
-st.markdown("*wulan"" is **really** **kalcer***.")
-st.markdown('''
-    :red[wulan] :orange[tek lan] :green[lan] :blue[wul] :violet[etek]
-    :gray[laan] :rainbow[sen] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
-
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
+number = st.number_input(
+    "Insert a number", value=None, placeholder="Type a number..."
+)
+st.write("The current number is ", number)
