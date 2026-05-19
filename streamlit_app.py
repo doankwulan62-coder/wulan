25e7246c4bd7960d2befb87e6c034f1537b916b5
doankwulan62-coder <doankwulan62@gmail.com>
2026-05-19 11:36:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,16 @@ number = st.number_input(
 st.write("The current number is ", number)
 import streamlit as st
 
+st.button("penambahan","pengurangan")
+if tambah st.button(+):
+    st.tambah(tambah)
+else:
+    st.write("Goodbye")
+
+if kurang st.button(-):
+    st.tambah(+)
+import streamlit as st
+
 number = st.number_input(
     "Insert a number", value=None, placeholder="Type a number..."
 )
